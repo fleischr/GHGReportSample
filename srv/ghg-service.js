90330b09285c8b45230903221ea8f626fb1f05db
Ryan Fleischmann <ryfleisch@gmail.com>
2024-03-18 22:17:54 -0400

    bit more tinkering with function import

--- srv/ghg-service.js
+++ srv/ghg-service.js
@@ -1,5 +1,5 @@
 const cds = require("@sap/cds");
-
+const vechain_utils = require("./vechain-utils");
 module.exports = cds.service.impl(async function() {
     this.on('offsetEmissions', async(emissionsID) => {
         console.log('offsetting emissions for' + emissionsID);
@@ -7,6 +7,7 @@ module.exports = cds.service.impl(async function() {
 
     this.on('tokenizeEmissions', async(req) =>{
         const { emissionsID } = req.data;
+        await vechain_utils.mintEmissionsNFT();
         return emissionsID;
     });
 });
--- srv/vechain-utils.js
+++ srv/vechain-utils.js
@@ -1,26 +1,31 @@
 import { Transaction } from "thor-devkit";
 
-async function mintEmissionsNFT(){
+var vechain_utils = {
+    mintEmissionsNFT : async function () {
 
-};
-
-async function addEmissionsData(){
-
-};
-
-function buildTransaction(clauses) {
-    // build the transaction
-const transaction = new Transaction({
-    chainTag: Number.parseInt(genesisBlock.id.slice(-2), 16),
-    blockRef: bestBlock.id.slice(0, 18),
-    expiration: 32,
-    clauses,
-    gas: bestBlock.gasLimit,
-    gasPriceCoef: 0,
-    dependsOn: null,
-    nonce: Date.now(),
-    reserved: {
-        features: 1
+    },
+    
+    addEmissionsData : async function (){
+    
+    },
+    
+    buildTransaction : function (clauses) {
+        // build the transaction
+        const transaction = new Transaction({
+            chainTag: Number.parseInt(genesisBlock.id.slice(-2), 16),
+            blockRef: bestBlock.id.slice(0, 18),
+            expiration: 32,
+            clauses,
+            gas: bestBlock.gasLimit,
+            gasPriceCoef: 0,
+            dependsOn: null,
+            nonce: Date.now(),
+            reserved: {
+                features: 1
+            }
+        });
     }
-});
-}
+};
+
+module.exports = vechain_utils;
+
