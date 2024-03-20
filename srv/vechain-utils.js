d33500c773f1c1bf726bc82e4bf1b883e7703627
Ryan Fleischmann <ryfleisch@gmail.com>
2024-03-19 22:45:00 -0400

    add more code for tokenization handling

--- srv/vechain-utils.js
+++ srv/vechain-utils.js
@@ -1,22 +1,44 @@
-import { Transaction } from "thor-devkit";
+const { Transaction } = require("thor-devkit");
 
 var vechain_utils = {
     mintEmissionsNFT : async function () {
+        console.log('minting emissions NFT...');
+        //todo get clauses
+        
+        //build transaction
+        const transaction = this.buildTransaction([]);
+        
+        // todo simulate
+        
+        // todo test for errors
+        
+        // todo fee delegation (ppbly option tbh)
+        
+        // sign the transaction
+        const signingHash = transaction.signingHash();
+        //const originSignature = secp256k1.sign(signingHash, Buffer.from(wallet.privateKey.slice(2), 'hex'));
+        //transaction.signature = Buffer.concat([originSignature.signature, sponsorSignature]);
 
+        //submit transaction
+        const rawTransaction = `0x${transaction.encode().toString('hex')}`;
+        // submit transaction pt 2
+        //const { id } = await post('/transactions', { raw: rawTransaction });
+        //console.log('Submitted with txId', id);
     },
     
     addEmissionsData : async function (){
     
     },
     
-    buildTransaction : function (clauses) {
+    buildTransaction : async function (clauses) {
         // build the transaction
+        const bestblock = await this.getBestBlock();
         const transaction = new Transaction({
             chainTag: Number.parseInt(genesisBlock.id.slice(-2), 16),
-            blockRef: bestBlock.id.slice(0, 18),
+            blockRef: bestblock.id.slice(0, 18),
             expiration: 32,
             clauses,
-            gas: bestBlock.gasLimit,
+            gas: bestblock.gasLimit,
             gasPriceCoef: 0,
             dependsOn: null,
             nonce: Date.now(),
@@ -24,6 +46,16 @@ var vechain_utils = {
                 features: 1
             }
         });
+        return transaction;
+    },
+
+    getBestBlock : async function () {
+        const thorestapi = await cds.connect.to('thorest');
+        const bestblock  = thorestapi.send({
+            method: 'GET',
+            path: '/blocks/best'
+        });
+        return bestblock;
     }
 };
 
