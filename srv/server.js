10b380a55c3127c84cdfedf457a9533c18d4f48c
Ryan Fleischmann <ryfleisch@gmail.com>
2024-06-11 23:54:19 -0400

    figuring out the swagger piece of this

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,14 @@
 const cds = require('@sap/cds')
+const cors = require('cors')
+const proxy = require('@sap/cds-odata-v2-adapter-proxy')
 
 cds.on('bootstrap', srv => {
     console.log('bootstrapping...')
+    srv.use(proxy())
+    srv.use(cors()) 
+    srv.get('/health', (_, res) => {
+        res.status(200).send('OK')
+    })
 })
 
 if(process.env.NODE_ENV != 'production') {
